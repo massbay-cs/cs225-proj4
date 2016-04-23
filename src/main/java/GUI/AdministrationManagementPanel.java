cc2c0556d0afebaba01c6e52dd0502bb570aa212
mdemilia1 <m_demilia1@post.massbay.edu>
2016-04-23 18:24:36 -0400

    all exceptions throws that are needed

--- src/main/java/GUI/AdministrationManagementPanel.java
+++ src/main/java/GUI/AdministrationManagementPanel.java
@@ -7,6 +7,10 @@ import javax.swing.table.DefaultTableModel;
 import BackEnd.ManagerSystem.MainManager;
 import BackEnd.UserSystem.Participant;
 import EMS_Database.DoesNotExistException;
+import auth.AuthorizationException;
+import auth.PrivilegeLevel;
+import exception.UpdateException;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,16 +33,12 @@ public class AdministrationManagementPanel extends javax.swing.JPanel implements
     private JButton submit;
 
 
-    public AdministrationManagementPanel() {
+    public AdministrationManagementPanel() throws DoesNotExistException, AuthorizationException {
         this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
         manage = new MainManager().getInstance();
         user = manage.getUserManager();
         userList = user.getUserList();
-        try {
-            setTable();
-        } catch (DoesNotExistException e) {
-            e.printStackTrace();
-        }
+        setTable();
 
         scrollBar = new JScrollPane(userTable);
         scrollBar.setVisible(true);
@@ -56,10 +56,20 @@ public class AdministrationManagementPanel extends javax.swing.JPanel implements
     {
         for(int i =0; i < ut.getRowCount(); i++)
         {
-            if((boolean) ut.getValueAt(i, 2) == true)
+            if((boolean) ut.getValueAt(i, 3) == true)
             {
                 System.out.println("row: " + i + " is checked");
                 //update the database on click
+                try {
+                    user.getUsersTable().setPrivilegeLevel((int )ut.getValueAt(i,2), PrivilegeLevel.ADMIN);
+                    System.out.println("is now an admin");
+                } catch (DoesNotExistException e1) {
+                    e1.printStackTrace();
+                } catch (AuthorizationException e1) {
+                    e1.printStackTrace();
+                } catch (UpdateException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
@@ -68,24 +78,26 @@ public class AdministrationManagementPanel extends javax.swing.JPanel implements
      * set up the table for all users that are not already admins
      * @throws DoesNotExistException
      */
-    public void setTable() throws DoesNotExistException {
+    public void setTable() throws AuthorizationException, DoesNotExistException {
         ut = new AdminPanelTable();
-        for (int j = 0; j < userList.size(); j++)
-        {
-            if(user.getUsersTable().getLevel(userList.get(j).getUserId()) == 0)
-                ut.addRow(new Object[]{userList.get(j).getFirstName(), userList.get(j).getLastName(), false});
+        for (int j = 0; j < userList.size(); j++) {
+            try {
+                if (user.getUsersTable().getPrivilegeLevel(userList.get(j).getUserId()) != PrivilegeLevel.ADMIN)
+
+                    ut.addRow(new Object[]{userList.get(j).getFirstName(), userList.get(j).getLastName(), userList.get(j).getUserId(), false});
+            }catch (DoesNotExistException dne){
+                throw new DoesNotExistException(""+dne.getMessage());
+            }
 
         }
         userTable = new JTable(ut);
         userTable.setPreferredScrollableViewportSize(new Dimension(50, 50));
         userTable.setFillsViewportHeight(true);
     }
-
-
     public class AdminPanelTable extends DefaultTableModel
     {
         public AdminPanelTable() {
-            super(new String[]{"First Name", "Last Name", "Make Admin"}, 0);
+            super(new String[]{"First Name", "Last Name", "UID", "Make Admin"}, 0);
         }
 
         public Class<?> getColumnClass(int columnIndex) {
